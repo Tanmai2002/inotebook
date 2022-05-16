ee2f33eb997a5f9522f147209d7bea31aafbd092
Tanmai Kamat <tanmaikamat@gmail.com>
2022-05-16 17:07:21 +0530

    express Validator added andd duplicacy handled

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,26 +3,33 @@ const User = require('../models/User');
 const router=express.Router()
 const { body, validationResult } = require('express-validator');
 
-
-router.post( '/',
-
+//POST request to create user . No login required
+router.post( '/createUser',
 [
     body('name','Atleast 3 character required in Name').isLength({min :3}),
     body('email','Invalid email').isEmail(),
     body('password','Password should be atleast of length 8').isLength({min :8})
-],(req,res)=>{
-console.log(req.body);
+],async (req,res)=>{
+    try{
 const errors=validationResult(req);
 if(!errors.isEmpty()){
     return res.status(400).json({errors : errors.array()});
 }
-User.create({
+let user= await User.findOne({email : req.body.email});
+if(user){
+    return res.status(400).json({error : "Email already registered."});
+}
+
+ user =await User.create({
     name: req.body.name,
     password: req.body.password,
     email : req.body.email
-  }).then(user => res.json(user)).catch((err)=>{
-        res.json({errors : 'Please enter another value for email. This email is already registered'});
   });
+  res.json({status :"User successfully created"});
+  
+}catch(error){
+    console.log(error);
+}
 // res.send("Received");
 });
 module.exports=router
