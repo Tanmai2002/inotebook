1a9daea01dcfb0d83e59943b3f302c754723f36e
Tanmai Kamat <tanmaikamat@gmail.com>
2022-06-03 13:04:47 +0530

    Api for notes Created

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const NoteSchema = new Schema({
+    user :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'user'
+    },
   title :{
       type : String,
       required :true
@@ -21,4 +25,6 @@ date :{
 },
 });
 
-module.exports=mongoose.model('note',NoteSchema);
+const Note =mongoose.model('note',NoteSchema);
+Note.createIndexes();
+module.exports=Note
